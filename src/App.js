3392fa3a4f60125d91bf1424db0dbff2ccf1c780
Serhii <beschasnuy96@gmail.com>
2022-08-16 23:59:42 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <button>Click Me!</button>
+      <div className='wrap'>
+        <div className='circle' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
