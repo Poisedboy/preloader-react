6c087db8d9e40ae9328f3e7cffa2ebabaf04136a
Serhii <beschasnuy96@gmail.com>
2022-08-17 00:32:46 +0300

    created animation with react-transition-group. Transition component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Transition } from 'react-transition-group';
 
 function App() {
+  const [loaderVisible, setLoaderVisible] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setLoaderVisible(true), 1000);
+    setTimeout(() => setLoaderVisible(false), 5000);
+  }, []);
   return (
     <div className="app">
-      <button>Click Me!</button>
+      <button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? 'HIDE' : 'SHOW'}</button>
       <div className='wrap'>
-        <div className='circle' />
+        <Transition
+          in={loaderVisible}
+          timeout={500}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+          onExit={() => console.log('onExit')}
+        >
+          {state => <div className={`circle ${state}`} />}
+        </Transition>
       </div>
     </div>
   );
